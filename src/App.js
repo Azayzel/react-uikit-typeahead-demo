06fd4a7c18336f5514260ecaadb7368c69d1a08a
Josh Lavely <jlavely@smartronix.com>
2019-10-18 15:36:00 -0400

    base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TypeAhead from 'react-uikit-typeahead';
+import UIKit from 'uikit'
+import 'uikit/dist/css/uikit.min.css'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      typeAheadSuggestions: ['josh', 'tony', 'max', 'amanda', 'john', 'jenny', 'alice'],
+      selected: ""
+    }
+  }
+
+  handleTypeAheadChange = (e) => {
+    this.setState({ selected: e });
+  }
+
+  render() {
+    return (
+      <div className="App uk-margin-medium">
+        <p className="uk-text-muted">Selected: {this.state.selected}</p>
+        <div className="uk-card uk-card-default uk-card-small uk-width-1-6 uk-align-center">
+          <div className="uk-form-controls">
+            <TypeAhead suggestions={this.state.typeAheadSuggestions} showSuggestions={true} handleTypeAheadChange={this.handleTypeAheadChange} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
