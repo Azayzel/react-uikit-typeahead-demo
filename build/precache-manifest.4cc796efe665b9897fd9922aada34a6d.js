05c7ec7559b05adc03d9311266ab0487e54a61ab
Josh Lavely <jlavely@smartronix.com>
2019-10-18 15:57:48 -0400

    new styling

--- build/precache-manifest.4cc796efe665b9897fd9922aada34a6d.js
+++ build/precache-manifest.4cc796efe665b9897fd9922aada34a6d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a441501a9ef34dd79712f645ecdc1679",
+    "revision": "94c693bd620ffaac2c06f046a3156f9a",
     "url": "/react-uikit-typeahead-demo/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-uikit-typeahead-demo/static/css/2.a88131bf.chunk.css"
   },
   {
-    "revision": "79ed5985d7c50de12992",
-    "url": "/react-uikit-typeahead-demo/static/css/main.fea89daf.chunk.css"
+    "revision": "b12a91f5625210c76288",
+    "url": "/react-uikit-typeahead-demo/static/css/main.5ee7fd49.chunk.css"
   },
   {
     "revision": "1f46155eee510dfcda82",
     "url": "/react-uikit-typeahead-demo/static/js/2.bd127d74.chunk.js"
   },
   {
-    "revision": "79ed5985d7c50de12992",
+    "revision": "b12a91f5625210c76288",
     "url": "/react-uikit-typeahead-demo/static/js/main.9142b2e8.chunk.js"
   },
   {
